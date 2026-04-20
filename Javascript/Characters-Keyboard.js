4a0adf59db802636bf54588a4dfe5b3db2dae302
Mod1025 <Ahmadmoh.1025@gmail.com>
2026-04-20 10:36:42 -0700

    updated

--- Javascript/Characters-Keyboard.js
+++ Javascript/Characters-Keyboard.js
@@ -1,6 +1,10 @@
+const charc = document.getElementById("player");
+
 /* Globle Varables*/ 
 let D = false;
 let A = false;
+let speed = 5;
+let posX = 0;
 
 // moving function //
 const e = (event) => {
@@ -9,22 +13,25 @@ const e = (event) => {
         A = true
     } else if (event.code === 'KeyD') {
         D = true
-    }
+    } 
 }
 // Stop moving function // 
 const Stopmoving = () => {
     A = false
     D = false
 }
+// Moving function //
+const moving = () => {
 
-let speed = 5;
-let posX = 0;
+    if (A === true) {
+        charc.style.left = (posX = posX - speed) + "px";
+    } else if (D === true) {
+        charc.style.left = (posx = posX + speed) + "px";
+    }
+    requestAnimationFrame(moving);
+}
 
-if (D) {
-    posX = posX + speed
-} else if (A) {
-    posX = posX - speed
-};
+moving()
 
 window.addEventListener('keydown', e );
 window.addEventListener('keyup', Stopmoving);
