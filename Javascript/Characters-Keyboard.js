e4e4983f0471bc9d174957c6ce97859ae6be0cd8
Mod1025 <Ahmadmoh.1025@gmail.com>
2026-04-28 10:42:41 -0700

    update

--- Javascript/Characters-Keyboard.js
+++ Javascript/Characters-Keyboard.js
@@ -1,35 +1,53 @@
 const charc = document.getElementById("player");
+const bubbles = document.getElementById("bubbles");
+const text =  document.getElementById("text-content");
 /* Globle Varables*/ 
 let D = false;
 let A = false;
 let speed = 5;
 let posX = 0;
 let posY = 0;
+let content = ["hi!", "Nice day.", "bye!"];
+let count = 0;
+let isTalking = false;
+let hasTalked = false;
 
 // moving function //
 const e = (event) => {
 
-    if (event.code === 'KeyA' || event.code === 'ArrowLeft') {
+    if ((event.code === 'KeyA' || event.code === 'ArrowLeft') && isTalking === false) {
         A = true
-    } else if (event.code === 'KeyD' || event.code === 'ArrowRight') {
+    } else if ((event.code === 'KeyD' || event.code === 'ArrowRight') && isTalking === false) {
         D = true
+    } else if (event.code === 'Enter' && isTalking === true) {
+        if (content.length <= count + 1) {
+            cleanup();
+        } else {
+            count++;
+            text.innerText = content[count];
+        }
+       
     }
 }
 
-
+const cleanup = () => {
+    bubbles.style.display = "none";
+    isTalking = false;
+    count = 0
+    A = false;
+    D = false;
+}
 // Stop moving function // 
 const Stopmoving = () => {
-    A = false
-    D = false
+        A = false
+        D = false
 }
 // Moving function //
 const moving = () => {
-
-
-    if (A === true && posX > 0) {
+    if (A === true && posX > 0 && isTalking === false) {
         charc.style.left = (posX = posX - speed) + "px";
         charc.style.transform = "scaleX(-1)";
-    } else if (D === true && posX < window.innerWidth - 245) {
+    } else if (D === true && posX < window.innerWidth - 245 && isTalking === false) {
         charc.style.left = (posX = posX + speed) + "px"; 
         charc.style.transform = "scaleX(1)";
     }
@@ -37,16 +55,22 @@ const moving = () => {
     requestAnimationFrame(moving);
 }
 
-function nextScene{
-    if charc
+const GateKeeper = () => {
     
+    if (posX > 700 && isTalking === false && hasTalked === false) {
+        isTalking = true;
+        A = false;
+        D = false;
+        bubbles.style.display = "block"
+        text.textContent = content[count];
+        hasTalked = true;
+    }
 
+    requestAnimationFrame(GateKeeper);
 }
 
-
-
+GateKeeper()
 moving()
 
 window.addEventListener('keydown', e );
 window.addEventListener('keyup', Stopmoving);
-window.addEventListener('click', nextScene)
