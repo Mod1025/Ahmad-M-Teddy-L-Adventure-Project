7eb5afae1456f30661b2afa402b04206f4d565a0
Mod1025 <Ahmadmoh.1025@gmail.com>
2026-05-07 10:41:37 -0700

    update

--- Combat System/Script.js
+++ Combat System/Script.js
@@ -7,7 +7,15 @@ const EHealth = document.getElementById("enemy-inner-Line");
 //Main vars //
 let playerHP = 100;
 let enemyHP = 100;
-
+let D = false;
+let A = false;
+let speed = 10;
+let posX = 0;
+let posY = 0;
+// Shooting vars // 
+let bulletX = -100;
+let isBulletActive = false;
+let bulletSpeed = 10;
 // player start point //
 let playerX = 0
 
@@ -22,38 +30,77 @@ enemy.style.left = enemyX + "px";
 // enemy movement fuction //
 let traget = 0;
 let moving = false; 
-let max = 800;
-let min = 100;
+let max = window.innerWidth / 2;
+let min = window.innerWidth - 200;
 const Emoving = () => {
-    let des = Math.floor(Math.random() *(max - min) + min)
+    let des = Math.floor(Math.random() * (max - min) + min)
     if (Math.abs(enemyX - des) < 200) {
        Emoving();
+       return;
     } else {
         traget = des;
     }   
     moving = true;
 }
 const Efacing = () => {
-    if (enemyX < traget) {
-        let Lposition = enemyX + enemySpeed
-        enemyX = Lposition
-        enemy.style.left = Lposition + "px";
-        enemy.style.transform = "scaleX(-1)";
-    } else if (enemyX > traget) {
-        let Rposition = enemyX - enemySpeed
-        enemyX = Rposition
-        enemy.style.left = Rposition + "px";
-        enemy.style.transform = "scaleX(1)";
-    }  
     if (Math.abs(enemyX - traget) < enemySpeed) {
         enemyX = traget
-        moving = "waiting"
-    }  
-    if (moving === "waiting") {
-        setTimeout( () => Emoving(), 1000) 
+        moving = "timerStarted"
+    } 
+    if (moving === true) {
+        if (enemyX < traget) {
+            let Lposition = enemyX + enemySpeed
+            enemyX = Lposition
+            enemy.style.left = Lposition + "px";
+            enemy.style.transform = "scaleX(-1)";
+        } else if (enemyX > traget) {
+            let Rposition = enemyX - enemySpeed
+            enemyX = Rposition
+            enemy.style.left = Rposition + "px";
+            enemy.style.transform = "scaleX(1)";     
+        }   
     }
-    requestAnimationFrame(Efacing);
+    if (moving === "timerStarted") {
+            setTimeout(() => Emoving(), 100) 
+            moving = true;
+        }
+    
+    requestAnimationFrame(Efacing);  
 }
 
+// Player movement // 
+const e = (event) => {
+
+    if (event.code === 'KeyA' || event.code === 'ArrowLeft') {
+        A = true
+    } else if (event.code === 'KeyD' || event.code === 'ArrowRight') {
+        D = true
+    }
+}
+
+const Stopmoving = () => {
+    A = false
+    D = false
+}
+
+const Pmoving = () => {
+if (A === true && posX > 0) {
+    player.style.left = (posX = posX - speed) + "px";
+    player.style.transform = "scaleX(1)";
+} else if (D === true && posX < window.innerWidth - 245) {
+    player.style.left = (posX = posX + speed) + "px"; 
+    player.style.transform = "scaleX(-1)";
+}
+
+requestAnimationFrame(Pmoving);
+}
+
+// Enemy shooting System // 
+
+
+
+Pmoving();
 Emoving();
 Efacing();
+window.addEventListener('keydown', e); 
+window.addEventListener('keyup', Stopmoving);
